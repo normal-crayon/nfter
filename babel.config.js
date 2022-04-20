b1e01e8cc2e7dcdd85c3882c27be2ddaa5d39243
normal-crayon <prithviraj.yuvaraj@gmail.com>
2022-04-20 08:11:44 +0530

    errors with webpack 5 and web3.js. commit has fixes i've tried. not working tho

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+// module.exports = function(api) {
+//   api.cache(true);
 
-  const presets = ["es2015", "stage-2", "stage-3"];
-  const plugins = ["@babel/plugin-syntax-import-meta", "@babel/plugin-proposal-class-properties"];
+//   const presets = ["es2015", "stage-2", "stage-3"];
+//   const plugins = ["@babel/plugin-syntax-import-meta", "@babel/plugin-proposal-class-properties"];
 
 
-  return{
-    presets, 
-    plugins
-  };
+//   return{
+//     presets, 
+//     plugins
+//   };
+// }
+module.exports = {
+  presets:["es2015", "stage-2", "stage-3"], 
+  overrides: [
+    {
+      // test: "./node_modules/@expo/next-adapter/document.js",
+      plugins: [["@babel/plugin-proposal-class-properties"]],
+    },
+  ],
 }
